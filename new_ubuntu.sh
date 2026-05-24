47a0d41a89381567003c1e5a6d6779ecc604228e
Code Devper <codedevper@gmail.com>
2026-05-25 04:30:42 +0700

    Update new_ubuntu.sh

--- new_ubuntu.sh
+++ new_ubuntu.sh
@@ -18,10 +18,14 @@ echo "======================================"
 echo " Install PHP Repository"
 echo "======================================"
 
+# Add the ondrej/php repository.
 sudo apt update
 sudo apt install -y software-properties-common
 sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
 sudo apt update
+
+# Install PHP.
+sudo apt install -y php8.5
 
 echo "======================================"
 echo " Install PHP ${PHP_VERSION}"
