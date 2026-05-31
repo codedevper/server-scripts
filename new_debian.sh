a1e0f5215d57f2fb1ff50cebeca01da9e707c891
Code Devper <codedevper@gmail.com>
2026-06-01 03:29:43 +0700

    Create new_debian.sh

---
+++ new_debian.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "======================================"
+echo " Update package"
+echo "======================================"
+
+sudo apt update && sudo apt upgrade -y
+
+echo "======================================"
+echo " Install PHP Repository"
+echo "======================================"
+
+# Add the packages.sury.org/php repository.
+sudo apt update
+sudo apt install -y lsb-release ca-certificates curl
+sudo curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+sudo dpkg -i /tmp/debsuryorg-archive-keyring.deb
+sudo tee /etc/apt/sources.list.d/php.sources <<EOF
+Types: deb
+URIs: https://packages.sury.org/php/
+Suites: $(lsb_release -sc)
+Components: main
+Signed-By: /usr/share/keyrings/debsuryorg-archive-keyring.gpg
+EOF
+
+sudo apt update
+
+# Install PHP.
+sudo apt install -y php8.5
+
+php -v
+
+echo "======================================"
+echo " Install Composer"
+echo "======================================"
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'c8b085408188070d5f52bcfe4ecfbee5f727afa458b2573b8eaaf77b3419b0bf2768dc67c86944da1544f06fa544fd47') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+sudo mv composer.phar /usr/local/bin/composer
+
+composer --version
+
+echo "======================================"
+echo " Install NVM"
+echo "======================================"
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+
+# in lieu of restarting the shell
+\. "$HOME/.nvm/nvm.sh"
+
+# Download and install Node.js:
+nvm install 24
+
+# Verify the Node.js version:
+node -v # Should print "v24.16.0".
+
+# Verify npm version:
+npm -v # Should print "11.13.0".
+
+echo "======================================"
+echo " Install Server"
+echo "======================================"
+
+sudo apt update
+sudo apt install sqlite3 -y
+sudo apt install mariadb-server mariadb-client -y
+sudo apt install redis-server -y
+sudo apt-get install -y \
+    libgd3 \
+    php8.5-cli \
+    php8.5-fpm \
+    php8.5-pgsql \
+    php8.5-sqlite3 \
+    php8.5-gd \
+    php8.5-curl \
+    php8.5-mongodb \
+    php8.5-imap \
+    php8.5-mysql \
+    php8.5-mbstring \
+    php8.5-xml \
+    php8.5-zip \
+    php8.5-bcmath \
+    php8.5-soap \
+    php8.5-intl \
+    php8.5-readline \
+    php8.5-ldap \
+    php8.5-msgpack \
+    php8.5-igbinary \
+    php8.5-redis \
+    php8.5-swoole \
+    php8.5-memcached \
+    php8.5-pcov \
+    php8.5-imagick \
+    php8.5-xdebug
+
+sqlite3 --version
+mysql --version
+redis-server --version
+
+echo "======================================"
+echo " Installation Complete"
+echo "======================================"
+echo ""
+echo "Please login and run:"
+echo ""
+echo "sudo mysql_secure_installation"
+echo "sudo apt install phpmyadmin"
+echo ""
+echo "to use mysql without sudo."
