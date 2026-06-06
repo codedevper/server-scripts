c37fc8bc47e23412fa0c272dec91685a2fe4a85b
Code Devper <codedevper@gmail.com>
2026-06-07 03:33:55 +0700

    Enhance database setup by adding safety checks
    Modified database creation script to safely drop existing database and user before creating them.

--- install-mysql.sh
+++ install-mysql.sh
@@ -10,7 +10,9 @@ sudo apt install -y mysql-server
 
 echo "==> Adding user to database..."
 sudo mysql
-CREATE DATABASE server;
+DROP DATABASE IF EXISTS server;
+CREATE DATABASE IF NOT EXISTS server;
+DROP USER IF EXISTS 'server'@'localhost;
 CREATE USER 'server'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON server.* TO 'server'@'localhost';
 FLUSH PRIVILEGES;
