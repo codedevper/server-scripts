66c6bfe4f76ba2280f492fd424eb4342bb6b8689
Code Devper <codedevper@gmail.com>
2026-06-07 08:22:25 +0700

    Add new_debian4.sh file

--- new_debian4.sh
+++ new_debian4.sh
@@ -18,11 +18,22 @@ echo "======================================"
 echo " Install PHP"
 echo "======================================"
 
-# Update the package lists.
+# Add the packages.sury.org/php repository.
+sudo apt update
+sudo apt install -y lsb-release ca-certificates curl
+sudo curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+sudo dpkg -i /tmp/debsuryorg-archive-keyring.deb
+sudo tee /etc/apt/sources.list.d/php.sources <<EOF
+Types: deb
+URIs: https://packages.sury.org/php/
+Suites: $(lsb_release -sc)
+Components: main
+Signed-By: /usr/share/keyrings/debsuryorg-archive-keyring.gpg
+EOF
 sudo apt update
 
 # Install PHP.
-sudo apt install -y php
+sudo apt install -y php8.4
 
 php -v
 
@@ -32,33 +43,33 @@ echo "======================================"
 
 sudo apt-get install -y \
     libgd3 \
-    php-cli \
-    php-dev \
-    php-pgsql \
-    php-sqlite3 \
-    php-gd \
-    php-curl \
-    php-mongodb \
-    php-imap \
-    php-mysql \
-    php-mbstring \
-    php-mcrypt \
-    php-xml \
-    php-zip \
-    php-bcmath \
-    php-bz2 \
-    php-soap \
-    php-intl \
-    php-readline \
-    php-ldap \
-    php-msgpack \
-    php-igbinary \
-    php-redis \
-    php-swoole \
-    php-memcached \
-    php-pcov \
-    php-imagick \
-    php-xdebug
+    php8.4-cli \
+    php8.4-dev \
+    php8.4-pgsql \
+    php8.4-sqlite3 \
+    php8.4-gd \
+    php8.4-curl \
+    php8.4-mongodb \
+    php8.4-imap \
+    php8.4-mysql \
+    php8.4-mbstring \
+    php8.4-mcrypt \
+    php8.4-xml \
+    php8.4-zip \
+    php8.4-bcmath \
+    php8.4-bz2 \
+    php8.4-soap \
+    php8.4-intl \
+    php8.4-readline \
+    php8.4-ldap \
+    php8.4-msgpack \
+    php8.4-igbinary \
+    php8.4-redis \
+    php8.4-swoole \
+    php8.4-memcached \
+    php8.4-pcov \
+    php8.4-imagick \
+    php8.4-xdebug
 
 echo "======================================"
 echo " Install Composer"
@@ -84,13 +95,13 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 
 # Download and install Node.js:
-nvm install 22
+nvm install 24
 
 # Verify the Node.js version:
-node -v # Should print "v22.22.3".
+node -v # Should print "v24.16.0".
 
 # Verify npm version:
-npm -v # Should print "10.9.8".
+npm -v # Should print "11.13.0".
 
 echo "======================================"
 echo " Install Server"
