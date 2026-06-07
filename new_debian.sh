d468449888d77c56be75e6f3d29aacb4bebde6fd
Code Devper <codedevper@gmail.com>
2026-06-07 16:09:49 +0700

    Update new_debian.sh

--- new_debian.sh
+++ new_debian.sh
@@ -96,11 +96,19 @@ echo " Install Composer"
 echo "======================================"
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'c8b085408188070d5f52bcfe4ecfbee5f727afa458b2573b8eaaf77b3419b0bf2768dc67c86944da1544f06fa544fd47') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
+EXPECTED_CHECKSUM="$(curl -fsSL https://composer.github.io/installer.sig)"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-sudo mv composer.phar /usr/local/bin/composer
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]; then
+        echo "ERROR: Invalid installer checksum"
+        rm -f composer-setup.php
+        exit 1
+    fi
+
+php composer-setup.php
+rm -f composer-setup.php
+
+mv composer.phar /usr/local/bin/composer
 
 composer --version
 
