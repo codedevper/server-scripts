24ed10f186d1759bbb451819e3ee7335c1896b69
Code Devper <codedevper@gmail.com>
2026-06-07 09:45:18 +0700

    Update Node.js and PHP installation in new_debian.sh
    Updated Node.js and PHP installation steps in the script. Added PHP 8.4 and its extensions while removing older PHP versions.

--- new_debian.sh
+++ new_debian.sh
@@ -18,8 +18,7 @@ echo "======================================"
 echo " Install Node"
 echo "======================================"
 
-# Download and install nvm:
-curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_24.x | bash -
 apt install -y nodejs
 
 echo "======================================"
@@ -39,38 +38,58 @@ echo "======================================"
 echo " Install PHP"
 echo "======================================"
 
-# Update the package lists.
+# Add the packages.sury.org/php repository.
+sudo apt update
+sudo apt install -y lsb-release ca-certificates curl
+sudo curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+sudo dpkg -i /tmp/debsuryorg-archive-keyring.deb
+sudo tee /etc/apt/sources.list.d/php.sources <<EOF
+Types: deb
+URIs: https://packages.sury.org/php/
+Suites: $(lsb_release -sc)
+Components: main
+Signed-By: /usr/share/keyrings/debsuryorg-archive-keyring.gpg
+EOF
 sudo apt update
 
 # Install PHP.
-sudo apt install -y php
+sudo apt install -y php8.4
+
+php -v
+
+echo "======================================"
+echo " Install multiple PHP extensions"
+echo "======================================"
 
 sudo apt-get install -y \
     libgd3 \
-    php-cli \
-    php-dev \
-    php-pgsql \
-    php-sqlite3 \
-    php-gd \
-    php-curl \
-    php-mongodb \
-    php-mysql \
-    php-mbstring \
-    php-xml \
-    php-zip \
-    php-bcmath \
-    php-soap \
-    php-intl \
-    php-readline \
-    php-ldap \
-    php-msgpack \
-    php-igbinary \
-    php-redis \
-    php-swoole \
-    php-memcached \
-    php-pcov \
-    php-imagick \
-    php-xdebug
+    php8.4-cli \
+    php8.4-dev \
+    php8.4-pgsql \
+    php8.4-sqlite3 \
+    php8.4-gd \
+    php8.4-curl \
+    php8.4-mongodb \
+    php8.4-imap \
+    php8.4-mysql \
+    php8.4-mbstring \
+    php8.4-mcrypt \
+    php8.4-xml \
+    php8.4-zip \
+    php8.4-bcmath \
+    php8.4-bz2 \
+    php8.4-soap \
+    php8.4-intl \
+    php8.4-readline \
+    php8.4-ldap \
+    php8.4-msgpack \
+    php8.4-igbinary \
+    php8.4-redis \
+    php8.4-swoole \
+    php8.4-memcached \
+    php8.4-pcov \
+    php8.4-imagick \
+    php8.4-xdebug
 
 echo "======================================"
 echo " Install Composer"
