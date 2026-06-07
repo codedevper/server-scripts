a6960cad1377efff1c131196bbe5b98ba1e39a2d
Code Devper <codedevper@gmail.com>
2026-06-07 17:59:43 +0700

    Simplify Node.js installation process
    Removed nvm installation and Node.js setup steps, replaced with direct Node.js installation via apt.

--- new_debian.sh
+++ new_debian.sh
@@ -88,20 +88,7 @@ echo "======================================"
 echo " Install Node"
 echo "======================================"
 
-# Download and install nvm:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
-
-# in lieu of restarting the shell
-\. "$HOME/.nvm/nvm.sh"
-
-# Download and install Node.js:
-nvm install 24
-
-# Verify the Node.js version:
-node -v # Should print "v24.16.0".
-
-# Verify npm version:
-npm -v # Should print "11.13.0".
+sudo apt install -y nodejs
 
 echo "======================================"
 echo " Install Server"
