3aa4a16d2b2ddfa843056917ec6e906dc011c025
Code Devper <codedevper@gmail.com>
2026-06-09 13:13:13 +0700

    Update new_ubuntu.sh

--- new_ubuntu.sh
+++ new_ubuntu.sh
@@ -13,3 +13,5 @@ echo " Install Dev Repository"
 echo "======================================"
 
 sudo apt install -y wget gnupg gosu curl ca-certificates zip unzip git supervisor sqlite3 libcap2-bin libpng-dev python3 dnsutils librsvg2-bin fswatch ffmpeg nano quota
+
+source ./lib/nodejs.sh
