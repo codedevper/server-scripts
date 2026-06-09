3e7d74e3ee00c583cad0d88c2600e7cd6ecc53a9
Code Devper <codedevper@gmail.com>
2026-06-09 12:52:49 +0700

    Update nodejs.sh

--- lib/nodejs.sh
+++ lib/nodejs.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/env bash
 
+set -e
+
+echo "======================================"
+echo " Update package"
+echo "======================================"
+
+sudo apt update && sudo apt upgrade -y
+
+echo "======================================"
+echo " Install Node"
+echo "======================================"
+
+curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
+sudo apt install -y nodejs
