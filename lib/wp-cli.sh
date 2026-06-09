19256862fa5e9b88c2ef49f5ceb03e67a92174e2
Code Devper <codedevper@gmail.com>
2026-06-09 12:54:38 +0700

    Update wp-cli.sh

--- lib/wp-cli.sh
+++ lib/wp-cli.sh
@@ -7,3 +7,14 @@ echo " Update package"
 echo "======================================"
 
 sudo apt update && sudo apt upgrade -y
+
+echo "======================================"
+echo " Install Wordpress CLI"
+echo "======================================"
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+wp --info
