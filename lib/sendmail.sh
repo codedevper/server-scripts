8eac9b0f802c642ec3d6aac370ed85480e10d94a
Code Devper <codedevper@gmail.com>
2026-06-09 12:53:59 +0700

    Update sendmail.sh

--- lib/sendmail.sh
+++ lib/sendmail.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
 
+set -e
+
+echo "======================================"
+echo " Update package"
+echo "======================================"
+
+sudo apt update && sudo apt upgrade -y
